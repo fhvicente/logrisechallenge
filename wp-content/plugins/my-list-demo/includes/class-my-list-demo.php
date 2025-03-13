a4918eae87288a5e62859a4aa2896f528692f14b
Flavio <flaviohenriquevicente@hotmail.com>
2025-03-13 09:37:12 +0000

    Admin page created

--- wp-content/plugins/my-list-demo/includes/class-my-list-demo.php
+++ wp-content/plugins/my-list-demo/includes/class-my-list-demo.php
@@ -3,13 +3,48 @@
  * The main plugin class
  */
 
- class My_List_Demo {
+class My_List_Demo {
 
     /**
      * Initialize the plugin
      */
 
-     public function run() {
+    public function run() {
+        // Register activation and deactivation hooks
+        register_activation_hook(plugin_dir_path(__DIR__) . 'my-list-demo.php', array($this, 'activate'));
+        register_deactivation_hook(plugin_dir_path(__DIR__) . 'my-list-demo.php', array($this, 'deactivation'));
+
+        add_action('admin-menu', array($this, 'add_admin_menu'));
+
+        // Register scripts and styles
+        add_action('admin_enqueue_scripts', array($this, 'enqueue_admin_assets'));
+        add_action('wp_enqueue_scripts', array($this, 'enqueue_frontend_assets'));
         
-     }
+    }
+
+    /**
+     * Add admin menu
+     */
+    public function add_admin_menu() {
+        add_menu_page(
+            __('My List Demo', 'my-list-demo'),
+            __('My List Demo', 'my-list-demo'),
+            'manage_options',
+            'my-list-demo',
+            array($this, 'render_admin_page'),
+            'dashicons-list-view',
+            30
+        );
+    }
+
+    /**
+     * Render admin page
+     */
+    public function render_admin_page() {
+        ?>
+        <div class="wrap">
+            <h1><?php echo esc_html__('My List Demo Settings', 'my-list-demo'); ?></h1>
+        </div>
+        <?php
+    }
  }
